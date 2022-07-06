5bb902948868f503dc83d5c562006dc7ab335c35
Tim Storm <tistorm@pothole.ahs.ri.cmu.edu>
2022-07-06 16:50:16 -0400

    added PR curve generation

--- benchmark_detector.py
+++ benchmark_detector.py
@@ -1,13 +1,9 @@
 import argparse
 import logging
 import os
+import sys
 
 import numpy as np
-import pandas as pd
-import sklearn.metrics
-import torch
-import seaborn as sns
-from PIL import Image
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
 from detectron2.data import build_detection_test_loader
@@ -17,17 +13,17 @@ from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.model_zoo import model_zoo
 from detectron2.modeling import build_model
 from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, balanced_accuracy_score
-from sklearn.metrics import precision_recall_curve
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
 
-from torchvision import transforms
-from torchvision.datasets import ImageFolder
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("benchmark")
 
 parser = argparse.ArgumentParser(description='Generate certain metrics for a given detection model and test dataset')
 
 parser.add_argument('-a', '--annotations',
                     required=True,
-                    help='Filepath to COCO annotations.')
+                    help='Filepath to COCO ground truth annotations.')
 
 parser.add_argument('-i', '--images',
                     required=True,
@@ -42,42 +38,100 @@ parser.add_argument('-o', '--output',
                     help='Filepath for results',
                     default='./benchmark/')
 
+parser.add_argument('--skip-evaluation',
+                    required=False,
+                    default=False,
+                    action='store_true',
+                    help="Will skip evaluation on the testset and instead just plot PR curve."
+                         "Requires coco_instances_results.json in output folder.")
+
+parser.add_argument('-p', '--predictions',
+                    required=False,
+                    help='Filepath to predictions in COCO format. '
+                         'Will be generated in output folder by default, only makes sense with --skip-evaluation',
+                    default="./benchmark/coco_instances_results.json")
+
 args = parser.parse_args()
 
 prev_dir = os.getcwd()
 os.makedirs(args.output, exist_ok=True)
 
-print("Loading test set.")
-register_coco_instances("test_data", {}, args.annotations, args.images)
-print("Test set loaded.")
-
-logging.basicConfig(level=logging.INFO)
-
-
-print("Loading model.")
-cfg = get_cfg()
-model = build_model(cfg)
-
-# Get Faster R-CNN model config we started out learning from
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
-# cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-
-cfg.MODEL.WEIGHTS = args.model_path
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
-
-DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
-print("Model loaded.")
+if not args.skip_evaluation:
+    logger.debug("Loading test set.")
+    register_coco_instances("test_data", {}, args.annotations, args.images)
+    logger.debug("Test set loaded.")
 
 
-CONFIDENCE = 0.5
+    logger.debug("Loading model.")
+    cfg = get_cfg()
+    model = build_model(cfg)
 
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1  # set a custom testing threshold
-predictor = DefaultPredictor(cfg)
+    # Get Faster R-CNN model config we started out learning from
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
+    # cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
 
-evaluator = COCOEvaluator("test_data", output_dir=cfg.OUTPUT_DIR)
-test_data_loader = build_detection_test_loader(cfg, "test_data")
-print(inference_on_dataset(predictor.model, test_data_loader, evaluator))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0
+    cfg.MODEL.WEIGHTS = args.model_path
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+
+    DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
+    logger.debug("Model loaded.")
+
+    predictor = DefaultPredictor(cfg)
+
+    evaluator = COCOEvaluator("test_data", output_dir=args.output)
+
+    test_data_loader = build_detection_test_loader(cfg, "test_data")
+    logger.info(inference_on_dataset(predictor.model, test_data_loader, evaluator))
+
+# Generate PR Curve
+# Load Ground Truth annotations
+logger.debug("Loading ground truth annotations.")
+cocoGt = COCO(args.annotations)
+logger.debug("Ground truth annotations loaded.")
+
+# load Detections in COCO format from file
+logger.debug("Loading detected bounding boxes.")
+if not os.path.exists(args.predictions):
+    logger.critical("Predictions could not be found. Ending program.")
+    sys.exit(1)
+
+cocoDt = cocoGt.loadRes(args.predictions)
+logger.debug("Detected bounding boxes loaded.")
+
+print("Running COCO evaluation")
+
+# Technically this is us doing everything twice, but we don't
+cocoEval = COCOeval(cocoGt, cocoDt, "bbox")
+cocoEval.params.imgIds = cocoGt.getImgIds()
+cocoEval.evaluate()
+cocoEval.accumulate()
+
+precision = cocoEval.eval["precision"]
+recall = cocoEval.eval["recall"]
+
+# we don't expect many detections per image, so might as well include the maximum of 100 (index for list [1, 10, 100])
+MAX_DETS = 2
+# we are interested in boxes regardless of their size, so we choose index 0 for "all" boxes
+AREA_INDEX = 0
+# we are class-agnostic and therefore have only one category anyways, so that one
+CATEGORY_ID = 0
+
+ious = np.arange(0.5, 1, 0.05)
+recall = np.arange(0, 1.01, 0.01)
+
+fig, ax = plt.subplots()
+
+for idx, iou in enumerate(ious):
+    ax.plot(recall, precision[idx, :, CATEGORY_ID, AREA_INDEX, MAX_DETS], label=f"IoU@{iou:.2f}")
+
+ax.legend(title='PR with different IoUs:')
+ax.set_xlabel("Recall")
+ax.set_ylabel("Accuracy")
+
+ax.set_title("PR Curve")
+
+plt.show()
 
 # go back to original directory
 os.chdir(prev_dir)
-

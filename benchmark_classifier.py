ea15781e9849e713d47ed7b326c7aa8d0b785f7e
Tim Storm <tistorm@pothole.ahs.ri.cmu.edu>
2022-07-11 19:09:26 -0400

    add option to show mismatches in grid for classifier benchmark

--- benchmark_classifier.py
+++ benchmark_classifier.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import os
 import sys
 
@@ -16,7 +17,8 @@ from sklearn.metrics import precision_recall_curve
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
-parser = argparse.ArgumentParser(description='Generate certain metrics for a given classification model and test dataset')
+parser = argparse.ArgumentParser(
+    description='Generate certain metrics for a given classification model and test dataset')
 
 parser.add_argument('-i', '--input',
                     required=True,
@@ -56,13 +58,15 @@ dataset_loader = torch.utils.data.DataLoader(folder, batch_size=16, shuffle=True
 
 class_names = ("Empty", "Full", "Garbage Bag")
 
+
 def imshow(img, title=None):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     if title:
         plt.title(title)
     plt.show()
+
 
 model = torch.load(args.model_path)
 model.eval()
@@ -84,9 +88,7 @@ for inputs, labels in dataset_loader:
     output = model(inputs)
 
     probabilities = torch.nn.functional.softmax(output, dim=1)
-    #print(probabilities)
     predictions = torch.max(probabilities, 1)
-    #print(predictions)
     # Save Prediction
     output = (torch.max(torch.exp(output), 1)[1]).data.cpu().numpy()
     y_pred.extend(output)
@@ -98,17 +100,35 @@ for inputs, labels in dataset_loader:
 ########################
 # SHOW FALSE PREDICTIONS
 ########################
+
+def plot_images_with_labels(images, labels):
+    """
+    Take list of image tensors (channel first!) and labels, plot as labelled grid
+    :param images:
+    :param labels:
+    """
+    nrow = 5
+    ncol = math.ceil(len(images) / nrow)
+    fig, m_axs = plt.subplots(nrow, ncol, figsize=(32, 32))
+    for (image, label, c_ax) in zip(images, labels, m_axs.flatten()):
+        c_ax.imshow(image.permute(1, 2, 0))
+        c_ax.set_title(label)
+        c_ax.axis('off')
+    plt.show()
+
+
 if args.show_failures:
     mismatches = []
     for idx, (output, truth) in enumerate(zip(y_pred, y_true)):
         if output != truth:
             mismatches.append((y_inputs[idx], output, truth))
 
-    print(len(mismatches))
-    imshow(
-        torchvision.utils.make_grid([m[0] for m in mismatches]),
-        title=[f"T:{class_names[truth]}|P:{class_names[pred]}" for _, truth, pred in mismatches]
+    plot_images_with_labels(
+        # unnormalize images
+        images=[(m[0] / 2 + 0.5) for m in mismatches],
+        labels=[f"T:{class_names[truth]}|P:{class_names[pred]}" for _, truth, pred in mismatches]
     )
+
 ####################
 # CONFUSION MATRIX
 ####################
@@ -117,7 +137,7 @@ if args.show_failures:
 # Build confusion matrix
 cf_matrix = confusion_matrix(y_true, y_pred)
 cf_matrix_normalized = cf_matrix.astype('float') / cf_matrix.sum(axis=1)[:, np.newaxis]
-fig, ax = plt.subplots(figsize=(10,10))
+fig, ax = plt.subplots(figsize=(10, 10))
 sns.heatmap(cf_matrix_normalized, annot=True, fmt='.2f',
             xticklabels=class_names, yticklabels=class_names)
 plt.title('Confusion matrix')
@@ -139,11 +159,9 @@ print()
 
 precisions, recalls, fbeta_scores, supports = precision_recall_fscore_support(y_true, y_pred, labels=[0, 1, 2])
 print(f"{'Label': <12}|{'Precision': <10}|{'Recall': <7}|{'Fbeta': <7}|{'Support': <7}")
-print('-'*47)
+print('-' * 47)
 for label, precision, recall, fbeta, support in zip(class_names, precisions, recalls, fbeta_scores, supports):
     print(f"{label: >12}|{precision: >10.2%}|{recall: >7.2%}|{fbeta: >7.2%}|{support: >7}")
 
-
 # go back to original directory
 os.chdir(prev_dir)
-

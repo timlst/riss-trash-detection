5e2bf14c61d89b226baeed762276899093c17421
Tim Storm <tistorm@pothole.ahs.ri.cmu.edu>
2022-07-13 14:38:09 -0400

    implemented custom Predictor for future adjustments

--- bounding_box_extractor.py
+++ bounding_box_extractor.py
@@ -6,16 +6,64 @@ import sys
 import warnings
 
 import numpy as np
+import torch
 from PIL import Image, ImageDraw
+from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
 from detectron2.engine import DefaultPredictor
 from detectron2.model_zoo import model_zoo
+from detectron2.modeling import build_model
+import detectron2.data.transforms as T
 
 from tqdm import tqdm
 
 # we dont want detectron2 loading logs
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("extraction")
+
+
+class MyPredictor:
+    def __init__(self, cfg):
+        self.cfg = cfg.clone()  # cfg can be modified by model
+        self.model = build_model(self.cfg)
+        self.model.eval()
+        if len(cfg.DATASETS.TEST):
+            self.metadata = MetadataCatalog.get(cfg.DATASETS.TEST[0])
+
+        # load weights
+        checkpointer = DetectionCheckpointer(self.model)
+        checkpointer.load(cfg.MODEL.WEIGHTS)
+
+        self.aug = T.ResizeShortestEdge(
+            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+        )
+
+        self.input_format = cfg.INPUT.FORMAT
+        assert self.input_format in ["RGB", "BGR"], self.input_format
+
+    def __call__(self, original_image):
+        """
+        Args:
+            original_image (np.ndarray): an image of shape (H, W, C) (in BGR order).
+
+        Returns:
+            predictions (dict):
+                the output of the model for one image only.
+                See :doc:`/tutorials/models` for details about the format.
+        """
+        with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
+            # Apply pre-processing to image.
+            if self.input_format == "RGB":
+                # whether the model expects BGR inputs or RGB
+                original_image = original_image[:, :, ::-1]
+            height, width = original_image.shape[:2]
+            image = self.aug.get_transform(original_image).apply_image(original_image)
+            image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+
+            inputs = {"image": image, "height": height, "width": width}
+            predictions = self.model([inputs])[0]
+            return predictions
 
 
 def _build_detection_model(model_path, config_file="COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml",
@@ -48,8 +96,7 @@ def _build_detection_model(model_path, config_file="COCO-Detection/faster_rcnn_R
 
     cfg.freeze()
 
-    # DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
-    predictor = DefaultPredictor(cfg)
+    predictor = MyPredictor(cfg)
 
     logger.debug("Model loaded.")
 

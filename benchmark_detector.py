9bbaf45a0c1ff766091cc181a1d617bdd7073680
Tim Storm <tistorm@pothole.ahs.ri.cmu.edu>
2022-08-03 12:52:02 -0400

    made detector benchmark more readable

--- benchmark_detector.py
+++ benchmark_detector.py
@@ -1,3 +1,6 @@
+"""
+A script that generates certain metrics for a given detection model and test dataset (in COCO format)
+"""
 import argparse
 import logging
 import os
@@ -5,20 +8,17 @@ import sys
 
 import numpy as np
 import torch
-from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.config import get_cfg
 from detectron2.data import build_detection_test_loader
 from detectron2.data.datasets import register_coco_instances
 from detectron2.engine import DefaultPredictor
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.model_zoo import model_zoo
-from detectron2.modeling import build_model
 from matplotlib import pyplot as plt
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
-from torch.utils.data import RandomSampler, DataLoader
 from tqdm import trange
-# in case we want to use RESNET18
+
+# in case we want to use RESNET18 or some other non-default backbones
+# reguires this: https://github.com/sxhxliang/detectron2_backbone
 from detectron2_backbone import backbone
 from detectron2_backbone.config import add_backbone_config
 
@@ -67,11 +67,11 @@ parser.add_argument('--skip-plot',
                     help="Will skip the plot"
                     )
 
-parser.add_argument('--run-benchmark',
-                    required=False,
-                    default=False,
-                    action='store_true',
-                    help="Will test inference time on the testset.")
+# parser.add_argument('--run-benchmark',
+#                     required=False,
+#                     default=False,
+#                     action='store_true',
+#                     help="Will test inference time on the testset.")
 
 parser.add_argument('-o', '--output',
                     required=False,
@@ -80,8 +80,15 @@ parser.add_argument('-o', '--output',
 
 args = parser.parse_args()
 
+# generate output directory and change back to original dir at the end
 prev_dir = os.getcwd()
 os.makedirs(args.output, exist_ok=True)
+
+########################################################################################################################
+# INITIALIZE MODEL
+# loads the detection model in case we will need it
+########################################################################################################################
+
 
 if not args.skip_evaluation or args.run_benchmark:
     # only initialize model and load data if required by one of the two
@@ -90,10 +97,10 @@ if not args.skip_evaluation or args.run_benchmark:
     logger.debug("Test set loaded.")
 
     logger.debug("Building model.")
-    # its necessary to set threshhold to 0 for COCO to have the full spectrum available (otherwise a default of 0.05
-    # is used)
+    # it's necessary to set threshold to 0 for COCO to have the full spectrum available (otherwise a default of 0.05
+    # is used, which introduces a little drop at the edge)
     cfg = utils.get_config_from_path(args.config)
-    cfg.MODEL.DEVICE = "cuda:0" # sometimes you wanna speed things up
+    cfg.MODEL.DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     predictor, cfg = _build_detection_model(
         cfg, weights_path=args.weights_path, additional_options=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", "0.0"]
     )
@@ -102,11 +109,20 @@ if not args.skip_evaluation or args.run_benchmark:
 
     test_data_loader = build_detection_test_loader(cfg, "test_data")
 
+########################################################################################################################
+# REDO EVALUATIONS ON TEST SET
+# if we aren't skipping evaluation (default), then we want to redo the inference on the test data set
+########################################################################################################################
 if not args.skip_evaluation:
     evaluator = COCOEvaluator("test_data", output_dir=args.output)
 
     logger.info(inference_on_dataset(predictor.model, test_data_loader, evaluator))
 
+
+########################################################################################################################
+# PRECISION RECALL CURVE
+# generates PR curve from the predictions file (either given as argument or generated in the previous step)
+########################################################################################################################
 # Generate PR Curve
 # Load Ground Truth annotations
 logger.debug("Loading ground truth annotations.")
@@ -131,22 +147,20 @@ cocoEval.params.imgIds = cocoGt.getImgIds()
 cocoEval.evaluate()
 cocoEval.accumulate()
 
+precision = cocoEval.eval["precision"]
 if not args.skip_plot:
-    precision = cocoEval.eval["precision"]
-
-
     # we don't expect many detections per image, so might as well include the maximum of 100 (index for list [1, 10, 100])
     MAX_DETS = 2
     # we are class-agnostic and therefore have only one category anyways, so that one
     CATEGORY_ID = 0
 
-    # ious = np.arange(0.5, 1, 0.05)
-
+    # what IoU steps are we interested in?
     ious = np.arange(0.5, 0.51, 0.05)
 
-    # we are interested in boxes regardless of their size, so we choose index 0 for "all" boxes
+    # what bounding box sizes should be considered? (give as indices for the list below)
     areas = [0, 3, ]
     area_labels = ["all", "small", "medium", "large"]
+
     recall = np.arange(0, 1.01, 0.01)
 
     fig, ax = plt.subplots()
@@ -167,8 +181,19 @@ if not args.skip_plot:
 
     plt.show()
 
+########################################################################################################################
+# CALCULATE AP50s/m/l
+# not part of standard COCO output, so we calculate them manually
+########################################################################################################################
+print("AP50sml")
+for area in [1, 2, 3]:
+    print(f"AP50{area_labels[area]}", np.average(precision[0, :, CATEGORY_ID, area, MAX_DETS]))
+
+
+########################################################################################################################
 # HIGHLIGHT FALSE NEGATIVES
-# we are interested in IoU50 as always
+# outputs all images where we NEVER find a bounding box in regardless of IoU
+########################################################################################################################
 
 uniques = set()
 for img in cocoEval.evalImgs:
@@ -179,37 +204,37 @@ for image_id, s in uniques:
     print(s, cocoGt.imgs.get(image_id)["file_name"])
 
 
-# Benchmark Inference time o
+# Benchmark Inference time over multiple runs - takes forever, there has to be a better way to do this
 
-def benchmark_inference_time(pred: DefaultPredictor, data, device):
-    dummy_input = torch.randn(1, 3, 224, 224, dtype=torch.float).to(device)
-    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
-    repetitions = 10
-    timings = np.zeros((repetitions, 1))
-
-    # MEASURE PERFORMANCE
-    with torch.no_grad():
-        for rep in trange(repetitions):
-            starter.record()
-            inference_on_dataset(pred.model, data, evaluator=None)
-            ender.record()
-            # WAIT FOR GPU SYNC
-            torch.cuda.synchronize()
-            curr_time = starter.elapsed_time(ender)
-            timings[rep] = curr_time
-
-    # Discard first result as they are warmup
-    timings = timings[1:]
-
-    mean_syn = np.sum(timings) / repetitions
-    std_syn = np.std(timings)
-    print(mean_syn)
-    print(std_syn)
-
-
-if args.run_benchmark:
-    print("Starting benchmark.")
-    benchmark_inference_time(predictor, test_data_loader, device="cuda:0")
+# def benchmark_inference_time(pred: DefaultPredictor, data, device):
+#     dummy_input = torch.randn(1, 3, 224, 224, dtype=torch.float).to(device)
+#     starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+#     repetitions = 10
+#     timings = np.zeros((repetitions, 1))
+#
+#     # MEASURE PERFORMANCE
+#     with torch.no_grad():
+#         for rep in trange(repetitions):
+#             starter.record()
+#             inference_on_dataset(pred.model, data, evaluator=None)
+#             ender.record()
+#             # WAIT FOR GPU SYNC
+#             torch.cuda.synchronize()
+#             curr_time = starter.elapsed_time(ender)
+#             timings[rep] = curr_time
+#
+#     # Discard first result as they are warmup
+#     timings = timings[1:]
+#
+#     mean_syn = np.sum(timings) / repetitions
+#     std_syn = np.std(timings)
+#     print(mean_syn)
+#     print(std_syn)
+#
+#
+# if args.run_benchmark:
+#     print("Starting benchmark.")
+#     benchmark_inference_time(predictor, test_data_loader, device="cuda:0")
 
 # go back to original directory
 os.chdir(prev_dir)
